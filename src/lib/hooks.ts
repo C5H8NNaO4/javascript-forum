b7c8b6fa65f9feb57d42607d28be083441f6031a
Moritz Roessler <morrossl@publicisgroupe.net>
2024-02-19 09:41:40 +0100

    fix: improve edit / save behaviour

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,22 +1,46 @@
 import { useEffect, useRef, useState } from 'react';
 
-export const useSyncedState = (defValue, updateFn, successFn?) => {
+export const useSyncedState = (
+  defValue,
+  updateFn,
+  {
+    draft,
+    successFn,
+  }: { draft?: boolean; successFn?: (value?: any) => void } = {
+    draft: false,
+  }
+) => {
   const timeout = useRef<any>(null);
+  const timestampSave = useRef(0);
+  const timestampLocal = useRef(0);
   const [localValue, setLocalValue] = useState(defValue);
   const [loading, setLoading] = useState(false);
+
   const setValue = async (value) => {
     setLocalValue(value);
-
+    timestampLocal.current = +new Date();
     clearTimeout(timeout.current);
-    timeout.current = setTimeout(async () => {
+    const save = async () => {
       setLoading(true);
+      timestampSave.current = +new Date();
       await updateFn(value);
       successFn?.(value);
-    }, 1500);
+    };
+
+    if (draft) {
+      await save();
+    } else {
+      timeout.current = setTimeout(async () => {
+        await save();
+      }, 1500);
+    }
   };
 
   useEffect(() => {
-    setLocalValue(defValue);
+    if (timestampLocal <= timestampSave) {
+      setLocalValue(defValue);
+    }
+
     setLoading(false);
   }, [defValue]);
 
