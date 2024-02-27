b7c3cc29c8ab4515a91e6bc28b445cb52c3a31b8
Moritz Roessler <morrossl@publicisgroupe.net>
2024-02-27 02:06:06 +0100

    feat: extract forum to @state-less/leap-frontend

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,37 @@
 import { Route } from 'react-router';
-import { CommunityPage } from './pages/community';
-import { PostsPage } from './pages/community/post';
-
+import { ForumPage, PostsPage } from '@state-less/leap-frontend';
 import HomeIcon from '@mui/icons-material/Home';
+
+import {
+  FORUM_BASE_PATH,
+  FORUM_KEY,
+  FORUM_QA_GH,
+  FORUM_RULES_GH,
+} from './lib/config';
 
 export const navigation = [['/', 'Home', '', 'Forum', HomeIcon]] as any;
 
 export const routes = [
   // <Route path="/" Component={MainPage} />,
-  <Route path="/" Component={CommunityPage} />,
-  <Route path="/:post" Component={PostsPage} />,
+  <Route
+    path="/"
+    Component={() => {
+      return (
+        <ForumPage
+          forumKey={FORUM_KEY}
+          basePath={FORUM_BASE_PATH}
+          ghSrc={{
+            rules: FORUM_RULES_GH,
+            qa: FORUM_QA_GH,
+          }}
+        />
+      );
+    }}
+  />,
+  <Route
+    path="/:post"
+    Component={() => {
+      return <PostsPage basePath={FORUM_BASE_PATH} forumKey={FORUM_KEY} />;
+    }}
+  />,
 ];
